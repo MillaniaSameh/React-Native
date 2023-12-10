90c510ef5dc8202800f086ca3ddd7f2ff312e0fd
Millania Sameh <samehmillania@gmail.com>
2023-12-10 15:53:38 +0200

    MealsApp initial layout

---
+++ MealsApp/screens/CategoriesScreen.js
@@ -0,0 +1,21 @@
+import { FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCatrgoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+export default function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCatrgoryItem}
+      numColumns={2}
+    />
+  );
+}
